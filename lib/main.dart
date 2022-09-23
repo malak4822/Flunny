8d979cd657c3f93157a6f3d1ca7873f4aab2535c
malak4822 <kmalak4822@gmail.com>
2022-09-23 09:20:07 +0200

    ios_first_commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flany/home.dart';
 import 'package:flany/loginpage.dart';
 import 'package:flany/providers/googlesignin.dart';
 import 'package:flany/providers/themes.dart';
 import 'package:flany/providers/zmienne.dart';
-import 'package:flany/settingspage.dart';
 import 'package:flany/userpage.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.ios);
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(create: (_) => GoogleSignInProvider()),
     ChangeNotifierProvider(create: (_) => ThemesProvider()),
